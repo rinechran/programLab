27335b010afcd5188c58799873ee246791db7cc2
rinechran <rinechran@outlook.jp>
2015-11-07 14:38:20 +0900

    cpp

--- Char/Char.cpp
+++ Char/Char.cpp
@@ -1,23 +1,34 @@
 #include "Char.hpp"
 
 
-Char::Char() :count(0){
+Char::Char() :count(0) {
+	this->p_char = nullptr;
 }
 
 
 Char::Char(char * oth_char) {
+
 	this->count = strlen(oth_char);
-	this->p_char = new char[this->count+1];
-	strcpy_s(this->p_char,this->count+1, oth_char);
-	
+	this->p_char = new char[this->count + 1];
+	strcpy_s(this->p_char, this->count + 1, oth_char);
+
+
+
+
 }
 Char::Char(const Char& oth_char) {
-	this->count = oth_char.count;
-	this->p_char = new char[this->count + 1];
-	strcpy_s(this->p_char, this->count + 1, oth_char.p_char);
+	if (oth_char.p_char!=nullptr) {
+		this->count = oth_char.count;
+		this->p_char = new char[this->count + 1];
+		strcpy_s(this->p_char, this->count + 1, oth_char.p_char);
+	}
+	else {
+		this->p_char = nullptr;
+		this->count = 0;
+	}
 }
 Char::~Char() {
-	delete [] this->p_char;
+	delete[] this->p_char;
 }
 int Char::length() const {
 	return this->count;
@@ -31,12 +42,12 @@ void Char::operator=(Char&  oth_char) {
 }
 Char  Char::operator+(Char &oth_char) {
 	int size = this->count + oth_char.count;
-	char* temp = new char[size+1];
+	char* temp = new char[size + 1];
 
 	strcpy_s(temp, size, this->p_char);
-	strcat_s(temp,size+1, oth_char.p_char);
+	strcat_s(temp, size + 1, oth_char.p_char);
 	Char k(temp);
-	delete []temp;
+	delete[]temp;
 	return k;
 
 }
