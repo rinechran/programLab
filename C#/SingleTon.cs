ba224a558e95992d313b1b72ea88cdac3b189a12
rinechran <rinechran@outlook.jp>
2016-07-25 19:48:44 +0900

    C# start
    start

---
+++ C#/SingleTon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationExample {
+    class Person {
+
+        public static Person Singleton = new Person();
+        private Person() {
+
+        }
+        public void write() {
+            Console.WriteLine("#1");
+        }
+
+
+    }
+    class Program {
+        static void Main( string[] args ) {
+            //ERROR because constructor private
+            //Person persion = new Person();
+            Person.Singleton.write();
+
+     
+        }
+    }
+}
