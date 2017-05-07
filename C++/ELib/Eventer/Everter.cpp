f2b5005d930cbb389344436095c9f6c6a3c944e7
rinechran <rinechran@outlook.jp>
2017-05-06 23:11:23 -0700

    Eventer
    Eventer

---
+++ C++/ELib/Eventer/Everter.cpp
@@ -0,0 +1,35 @@
+﻿#include <vector>
+#include <queue>
+#include <memory>
+#include <thread>
+#include <future>
+#include <functional>
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class Eventer {
+public:
+
+	Eventer() = default;
+
+	template <typename Func,typename ...Args>
+	void AddListener(Func && func, Args && ... args) {
+		auto tesk = [&]()->void {func(args...);  };
+		event.push_back(Node {tesk} );
+	}
+	void start() {
+		for (auto &i = event.begin(); i != event.end();++i) {
+			i->func();
+		}
+	}
+
+private:
+	struct Node {
+		std::function<void()> func;
+	};
+	std::vector<Node> event;
+
+};
+
